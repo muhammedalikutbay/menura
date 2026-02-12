01f2f2b53e93cb2be22ed35df9dc0c916fed5a2e
Muhammed Ali KUTBAY <muhammedalikutbay@gmail.com>
2026-02-13 00:00:03 +0300

    feat: input component iskeleti oluşturuldu

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant?: "default" | "error";
+  size?: "sm" | "md" | "lg";
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      variant = "default",
+      size = "md",
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <input
+        ref={ref}
+        className={className}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
